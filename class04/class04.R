7497db98e7109ecf262923dd97b3863727e0517c
Amanda Wilpitz <awilpitz@ucsd.edu>
2025-02-25 11:36:43 -0800

    Added courses

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Amanda Wilpitz (A17463962)"
+#' date: "January 16, 2025"
+#' ---
+
+
+x <- 1:50
+plot(x, sin(x))
+
+#Make this nicer
+plot(x, sin(x), col="blue", type="l", lwd=3)
